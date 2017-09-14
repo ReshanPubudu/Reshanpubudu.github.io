c3b3cf854b7db4c3549a8f8c88b14661ac34b2c4
ReshanPubudu <31838065+ReshanPubudu@users.noreply.github.com>
2017-09-15 02:25:44 +0530

    3rd shy
    update version

--- js/Controllers.js
+++ js/Controllers.js
@@ -10,25 +10,41 @@ var tempGuested =[];
 var guested = [];
 var rodBlck =[];
 var clicked = null;
+var moving = "White";
 
 function gestWhitePawn(ev) {
     var uni= ev[0];
     var num = ev[1];
-    var g = 0,rb = 0; tempGuested =[]; rodBlck =[];
+    var g=0, tg=0, rb=0; tempGuested =[]; rodBlck =[]; guested = [];
     var cnt = 0;
     $("#"+String.fromCharCode(uni)+"-"+ num).css("background-color","rgba(181,255,30,0.67)");
     if(num==2) cnt=num+2; else cnt=num+1;
     L1:while(num++!=cnt){
-        var id = "#"+String.fromCharCode(uni)+"-"+ num;
+        var guestId = "#"+String.fromCharCode(uni)+"-"+ num;
         for(var x =0;x<peases.length;x++){
-            if(peases[x]==id){
-                rodBlck[rb++] = id;
-                continue L1;
+            var peas=idBreaker(peases[x]);
+            if(peases[x]==guestId){
+                if(rodBlck[0]==null)rodBlck[0] = peases[x];
+                else {
+                    var rdBlk=idBreaker(rodBlck[0]);
+                    if (rdBlk[1]>num) rodBlck[0] = peases[x];
+                }
+            }
+            if((peas[0]==uni-1&&peas[1]==num)||(peas[0]==uni+1&&peas[1]==num)){
+                tempGuested[tg++]=peases[x];
             }
         }
-        $(id).css("background-color","rgba(181,255,30,0.67)");
-        tempGuested[g++] = id;
-    }guested = tempGuested;
+        tempGuested[tg++] = guestId;
+    }
+    console.log(tempGuested);
+    for(var i=0;i<rodBlck.length;i++){
+        for(var j=0;j<tempGuested.length;j++){
+            var rdBlk=idBreaker(rodBlck[i]);
+            var tempGust=idBreaker(tempGuested[j]);
+            if(tempGust[1]<=rdBlk[1]) guested[g++]=tempGuested[j];
+        }
+    }
+    selectAllMovedSqrs();
 }
 
 function gestBlackPawn(ev) {
@@ -39,43 +55,111 @@ function gestBlackPawn(ev) {
     $("#"+String.fromCharCode(uni)+"-"+ num).css("background-color","rgba(181,255,30,0.67)");
     if(num==7) cnt=num-2; else cnt=num-1;
     L1:while(num--!=cnt){
-        var id = "#"+String.fromCharCode(uni)+"-"+ num;
+        var guestId = "#"+String.fromCharCode(uni)+"-"+ num;
         for(var x =0;x<peases.length;x++){
-            if(peases[x]==id){
-                rodBlck[rb++] = id;
+            if(peases[x]==guestId){
+                rodBlck[rb++] = guestId;
+                if(rb==2) break;
                 continue L1;
             }
         }
-        $(id).css("background-color","rgba(181,255,30,0.67)");
-        tempGuested[g++] = id;
+        $(guestId).css("background-color","rgba(181,255,30,0.67)");
+        tempGuested[g++] = guestId;
     }guested = tempGuested;
 }
 
 function gestRook(ev) {
     var uni= ev[0];
     var num = ev[1];
-    var g = 0,rb = 0; tempGuested =[]; rodBlck =[];
+    var g = 0,trb = 0;
+    var tempRodBlk =[]; tempGuested =[];
+    rodBlck =["#"+String.fromCharCode(uni)+"-8","#a-"+num,"#"+String.fromCharCode(uni)+"-1","#h-"+num];
     for(var i=97;i<105;i++){
         L1:for(var j=1;j<9;j++){
             var id = (i==uni || j==num ? ("#"+String.fromCharCode(i)+"-"+j):null);
             if(id!=null){
                 for(var x =0;x<peases.length;x++){
                     if(peases[x]==id&&id!="#"+String.fromCharCode(uni)+"-"+num){
-                        rodBlck[rb++] = id;
-                        continue L1;
+                        tempRodBlk[trb++] = id;
                     }
                 }
                 if(id!="#"+String.fromCharCode(uni)+"-"+num) tempGuested[g++] = id;
             }
         }
     }
+    for(var i=0;i<tempRodBlk.length;i++){
+        var tempRdBlk=idBreaker(tempRodBlk[i]);
+        var topRdBlk=idBreaker(rodBlck[0]);
+        var leftRdBlk=idBreaker(rodBlck[1]);
+        var botmRdBlk=idBreaker(rodBlck[2]);
+        var rightRdBlk=idBreaker(rodBlck[3]);
+        if(tempRdBlk[0]==uni&&tempRdBlk[1]>num&&tempRdBlk[1]<topRdBlk[1]) rodBlck[0]=tempRodBlk[i];
+        if(tempRdBlk[0]<uni&&tempRdBlk[1]==num&&tempRdBlk[0]>leftRdBlk[0]) rodBlck[1]=tempRodBlk[i];
+        if(tempRdBlk[0]==uni&&tempRdBlk[1]<num&&tempRdBlk[0]>leftRdBlk[0]) rodBlck[2]=tempRodBlk[i];
+        if(tempRdBlk[0]>uni&&tempRdBlk[1]==num&&tempRdBlk[0]<rightRdBlk[0]) rodBlck[3]=tempRodBlk[i];
+    }
     gestRookMoved("#"+String.fromCharCode(uni)+"-"+num);
+    selectAllMovedSqrs();
+}
+
+function gestBishop(ev) {
+    var uni= ev[0];
+    var num = ev[1];
+    var g = 0,trb = 0;
+    var tempRodBlk =[]; tempGuested =[];
+    rodBlck =[
+        (uni!=num+96?uni>num+96?("#h-"+(104-uni+num)):("#"+String.fromCharCode(8-num+uni)+"-8"):"#h-8"),
+        (uni!=105-num?uni<105-num?("#a-"+(uni-97+num)):("#"+String.fromCharCode(uni+num-8)+"-8"):"#a-8"),
+        (uni!=num+96?uni<num+96?("#a-"+(num-uni+97)):("#"+String.fromCharCode(uni-num+1)+"-1"):"#a-1"),
+        (uni!=105-num?uni>105-num?("#h-"+(num+uni-104)):("#"+String.fromCharCode(uni+num-1)+"-1"):"#h-1")];
+    for(var i=97;i<105;i++){
+        L1:for(var j=1;j<9;j++){
+            var id =((uni-i==num-j)||(uni-i==-(num-j))? ("#"+String.fromCharCode(i)+"-"+j):null);
+            if(id!=null){
+                for(var x =0;x<peases.length;x++){
+                    if(peases[x]==id&&id!="#"+String.fromCharCode(uni)+"-"+num){
+                        tempRodBlk[trb++] = id;
+                    }
+                }
+                if(id!="#"+String.fromCharCode(uni)+"-"+num) tempGuested[g++] = id;
+            }
+        }
+    }
+    for(var i=0;i<tempRodBlk.length;i++){
+        var tempRdBlk=idBreaker(tempRodBlk[i]);
+        var writeTopPart=idBreaker(rodBlck[0]);
+        var lefttopPart=idBreaker(rodBlck[1]);
+        var leftBtomPart=idBreaker(rodBlck[2]);
+        var rightBtmPart=idBreaker(rodBlck[3]);
+        if(tempRdBlk[0]>=uni&&tempRdBlk[1]>=num&&tempRdBlk[0]<=writeTopPart[0]&&tempRdBlk[1]<=writeTopPart[1]) rodBlck[0]=tempRodBlk[i];
+        if(tempRdBlk[0]<=uni&&tempRdBlk[1]>=num&&tempRdBlk[0]>=lefttopPart[0]&&tempRdBlk[1]<=lefttopPart[1]) rodBlck[1]=tempRodBlk[i];
+        if(tempRdBlk[0]<=uni&&tempRdBlk[1]<=num&&tempRdBlk[0]>=leftBtomPart[0]&&tempRdBlk[1]>=leftBtomPart[1]) rodBlck[2]=tempRodBlk[i];
+        if(tempRdBlk[0]>=uni&&tempRdBlk[1]<=num&&tempRdBlk[0]<=rightBtmPart[0]&&tempRdBlk[1]>=rightBtmPart[1]) rodBlck[3]=tempRodBlk[i];
+    }
+    gestBishopMoved("#"+String.fromCharCode(uni)+"-"+num);
+    selectAllMovedSqrs();
+}
+
+function gestQueen(ev){
+    gestRook(ev);
+    var guest1=guested;
+    gestBishop(ev);
+    var guest2=guested;
+    var g=0; guested = [];
+    for(var i=0;i<guest1.length;i++){
+        guested[g++]=guest1[i];
+    }
+    for(var i=0;i<guest2.length;i++){
+        guested[g++]=guest2[i];
+    }
 }
 
 function gestKnight(ev){
     var uni= ev[0];
     var num = ev[1];
-    var g = 0,rb = 0; tempGuested =[]; rodBlck =[];
+    var g = 0,rb = 0;
+    var tempRodBlk =[]; tempGuested =[];
+    rodBlck =[];
     for(var i=97;i<105;i++){
         L1:for(var j=1;j<9;j++){
             var id =((i==uni+2&&j==num+1)||(i==uni+1&&j==num+2)||(i==uni+2&&j==num-1)||(i==uni+1&&j==num-2)||(i==uni-2&&j==num+1)||(i==uni-1&&j==num+2)||(i==uni-2&&j==num-1)||(i==uni-1&&j==num-2)||("#"+String.fromCharCode(i)+"-"+j=="#"+String.fromCharCode(uni)+"-"+num)?("#"+String.fromCharCode(i)+"-"+j):null);
@@ -83,55 +167,10 @@ function gestKnight(ev){
                 for(var x =0;x<peases.length;x++){
                     if(peases[x]==id&&id!="#"+String.fromCharCode(uni)+"-"+num){
                         rodBlck[rb++] = id;
-                        continue L1;
                     }
                 }
                 $(id).css("background-color","rgba(181,255,30,0.67)");
                 if(id!="#"+String.fromCharCode(uni)+"-"+num) tempGuested[g++] = id;
-            }
-        }
-    }guested = tempGuested;
-}
-
-function gestBishop(ev) {
-    var uni= ev[0];
-    var num = ev[1];
-    var g = 0,rb = 0; tempGuested =[]; rodBlck =[];
-    for(var i=97;i<105;i++){
-        L1:for(var j=1;j<9;j++){
-            var id =((uni-i==num-j)||(uni-i==-(num-j))? ("#"+String.fromCharCode(i)+"-"+j):null);
-            if(id!=null){
-                for(var x =0;x<peases.length;x++){
-                    if(peases[x]==id&&id!="#"+String.fromCharCode(uni)+"-"+num){
-                        rodBlck[rb++] = id;
-                        continue L1;
-                    }
-                }
-                // $(id).css("background-color", "rgba(181,255,30,0.67)");
-                if(id!="#"+String.fromCharCode(uni)+"-"+num) tempGuested[g++] = id;
-            }
-        }
-    }
-    // guested = tempGuested;
-    gestBishopMoved("#"+String.fromCharCode(uni)+"-"+num);
-}
-console
-function gestQueen(ev){
-    var uni= ev[0];
-    var num = ev[1];
-    var g = 0,rb = 0; tempGuested =[]; rodBlck =[];
-    for(var i=97;i<105;i++) {
-        L1:for (var j = 1; j < 9; j++) {
-            var id = (i==uni||j==num||(uni-i==num-j)||(uni-i==-(num-j))?("#"+String.fromCharCode(i)+"-"+j):null);
-            if (id != null) {
-                for (var x = 0; x < peases.length; x++) {
-                    if(peases[x]==id&&id!="#"+String.fromCharCode(uni)+"-"+num){
-                        rodBlck[rb++] = id;
-                        continue L1;
-                    }
-                }
-                $(id).css("background-color", "rgba(181,255,30,0.67)");
-                if (id != "#" + String.fromCharCode(uni) + "-" + num) tempGuested[g++] = id;
             }
         }
     }guested = tempGuested;
@@ -148,7 +187,6 @@ function gestKing(ev) {
                 for(var x =0;x<peases.length;x++){
                     if(peases[x]==id&&id!="#"+String.fromCharCode(uni)+"-"+num){
                         rodBlck[rb++] = id;
-                        continue L1;
                     }
                 }
                 $(id).css("background-color","rgba(181,255,30,0.67)");
@@ -224,32 +262,33 @@ function mouseClick(ev) {
             }
         }
         guested= [];
+        if(moving=="White") moving = "Black";
+        else moving = "White";
     }
 }
 
 function idBreaker(ev){
-    var string= ev.match(/[A-z]/g).join("");
-    var uni = parseInt(string.charCodeAt(0));
-    var num= parseInt(ev.match(/\d/g));
-    return [uni,num];
+    if(ev!="") {
+        var string = ev.match(/[A-z]/g).join("");
+        var uni = parseInt(string.charCodeAt(0));
+        var num = parseInt(ev.match(/\d/g));
+        return [uni, num];
+    }else return [96, 0];
 }
 
 function gestRookMoved(ev) {
     var id = idBreaker(ev);
-    var topBlk = 9, botmBlk= 0, left = 96,right = 105, g =0; guested = [];
+    var g =0; guested = [];
     for(var i=0;i<rodBlck.length;i++) {
-        var blckId = idBreaker(rodBlck[i]);
-        if (id[1] < blckId[1] && topBlk >= blckId[1]) topBlk = blckId[1];
-        if (id[1] > blckId[1] && botmBlk <= blckId[1]) botmBlk = blckId[1];
-        if (id[0] > blckId[0] && left <= blckId[0]) left = blckId[0];
-        if (id[0] < blckId[0] && right >= blckId[0]) right = blckId[0];
+        var rdBlk = idBreaker(rodBlck[i]);
+        for(var j=0;j<tempGuested.length;j++){
+            var tempId = idBreaker(tempGuested[j]);
+            if(i==0&&rdBlk[1]>=tempId[1]&&tempId[1]>id[1]) guested[g++] = tempGuested[j];
+            else if(i==1&&rdBlk[0]<=tempId[0]&&tempId[0]<id[0]) guested[g++] = tempGuested[j];
+            else if(i==2&&rdBlk[1]<=tempId[1]&&tempId[1]<id[1]) guested[g++] = tempGuested[j];
+            else if(i==3&&rdBlk[0]>=tempId[0]&&tempId[0]>id[0]) guested[g++] = tempGuested[j];
+        }
     }
-    for(var j=0;j<tempGuested.length;j++){
-        var tempId = idBreaker(tempGuested[j]);
-        if((id[0]==tempId[0])&&(tempId[1]<topBlk&&tempId[1]>botmBlk)) guested[g++]=tempGuested[j];
-        else if((id[1]==tempId[1])&&(tempId[0]<right&&tempId[0]>left)) guested[g++]=tempGuested[j];
-    }
-    selectAllMovedSqrs();
 }
 
 function gestBishopMoved(ev) {
@@ -259,15 +298,12 @@ function gestBishopMoved(ev) {
         var rdBlk=idBreaker(rodBlck[i]);
         for(var j=0;j<tempGuested.length;j++){
             var tempId = idBreaker(tempGuested[j]);
-            if(rdBlk[0]>id[0]&&rdBlk[1]>id[1]&&tempId[0]<rdBlk[0]&&tempId[1]<rdBlk[1]&&tempId[0]>id[0]&&tempId[1]>id[1]) guested[g++]=tempGuested[j];
-            else if(rdBlk[0]<id[0]&&rdBlk[1]>id[1]&&tempId[0]>rdBlk[0]&&tempId[1]<rdBlk[1]&&tempId[0]<id[0]&&tempId[1]>id[1]) guested[g++]=tempGuested[j];
-            else if(rdBlk[0]<id[0]&&rdBlk[1]<id[1]&&tempId[0]>rdBlk[0]&&tempId[1]>rdBlk[1]&&tempId[0]<id[0]&&tempId[1]<id[1]) guested[g++]=tempGuested[j];
-            else if(rdBlk[0]>id[0]&&rdBlk[1]<id[1]&&tempId[0]<rdBlk[0]&&tempId[1]>rdBlk[1]&&tempId[0]>id[0]&&tempId[1]<id[1]) guested[g++]=tempGuested[j];
-            else if((tempId[0]>id[0]&&tempId[1]>id[1])&&(rdBlk[0]>id[0]&&rdBlk[1]>id[1]))/*||(tempId[0]<id[0]&&tempId[1]>id[1])||(tempId[0]<id[0]&&tempId[1]<id[1])||(tempId[0]>id[0]&&tempId[1]<id[1]))*/ guested[g++]=tempGuested[j];
+            if(i==0&&(tempId[0]<=rdBlk[0]&&tempId[1]<=rdBlk[1]&&tempId[0]>id[0]&&tempId[1]>id[1])) guested[g++] = tempGuested[j];
+            else if(i==1&&(tempId[0]>=rdBlk[0]&&tempId[1]<=rdBlk[1]&&tempId[0]<id[0]&&tempId[1]>id[1])) guested[g++] = tempGuested[j];
+            else if(i==2&&(tempId[0]>=rdBlk[0]&&tempId[1]>=rdBlk[1]&&tempId[0]<id[0]&&tempId[1]<id[1])) guested[g++] = tempGuested[j];
+            else if(i==3&&(tempId[0]<=rdBlk[0]&&tempId[1]>=rdBlk[1]&&tempId[0]>id[0]&&tempId[1]<id[1])) guested[g++] = tempGuested[j];
         }
     }
-    selectAllMovedSqrs();
-    console.log("select "+guested);
 }
 
 function selectAllMovedSqrs() {
